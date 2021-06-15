c038769f875603e44f7c2d7b81f2c5715d03fa7e
Arthur Groupp <arth.groupp@gmail.com>
2021-06-15 16:46:36 +0300

    initial commit

---
+++ apps/web-lib-customer/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'web-lib-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'web-lib-customer';
+}
